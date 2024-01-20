5d34f10fa3f52eb86b9bb0f1f917a72de7164e53
lkendi <lkendi003@gmail.com>
2024-01-20 19:57:03 +0300

    Add monty operation functions

--- monty_operations.c
+++ monty_operations.c
@@ -60,11 +60,83 @@ void sub(stack_t **stack, unsigned int line_number)
 
 	if (*stack == NULL || (*stack)->next == NULL)
 	{
-		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
+		fprintf(stderr, "L%d: can't sub, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 
 	temp = (*stack)->next;
 	temp->n -= (*stack)->n;
+	pop(stack, line_number);
+}
+
+/**
+ * div - divides the second top element of the stack by the top element of the stack
+ * @stack: pointer to pointer to the top of the stack
+ * @line_number: instruction line number
+ * Return: nothing
+*/
+void _div(stack_t **stack, unsigned int line_number)
+{
+	stack_t *temp;
+
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%d: can't div, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	if ((*stack)->n == 0)
+	{
+		fprintf(stderr, "L%d: division by zero\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	temp = (*stack)->next;
+	temp->n /= (*stack)->n;
+	pop(stack, line_number);
+}
+
+/**
+ * mul - divides the second top element of the stack by the top element of the stack
+ * @stack: pointer to pointer to the top of the stack
+ * @line_number: instruction line number
+ * Return: nothing
+*/
+void mul(stack_t **stack, unsigned int line_number)
+{
+	stack_t *temp;
+
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%d: can't mul, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	temp = (*stack)->next;
+	temp->n *= (*stack)->n;
+	pop(stack, line_number);
+}
+
+/**
+ * mod - computes the rest of the division of the second top element of the stack by the top element of the stack.
+ * @stack: pointer to pointer to the top of the stack
+ * @line_number: instruction line number
+ * Return: nothing
+*/
+void mod(stack_t **stack, unsigned int line_number)
+{
+	stack_t *temp;
+
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%d: can't mod, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	if ((*stack)->n == 0)
+	{
+		fprintf(stderr, "L%d: division by zero\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	temp = (*stack)->next;
+	temp->n %= (*stack)->n;
 	pop(stack, line_number);
 }
